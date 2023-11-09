d360e2e65af25f873a30c1e11cd79fbcbe496120
Tanishksoam <soamtanishk@gmail.com>
2023-11-09 11:43:43 +0530

    Primary Grid

--- super-tic-tac-toe/src/App.jsx
+++ super-tic-tac-toe/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
+import Arena from "./components/Arena";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className=" m-0 p-10 bg-teal-500 w-screen h-screen">
       <Header />
+      <Arena />
     </div>
   );
 }
---
+++ super-tic-tac-toe/src/components/PrimBox.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PrimBox = () => {
+  return <div className="w-48 h-48 p-10 bg-teal-500"></div>;
+};
+
+export default PrimBox;
